12264eca9445eb68ab26f94bcf66729a250bce61
SunSeekerX <1647800606@qq.com>
2021-12-25 18:31:39 +0800

    feat: feat

--- packages/crypto-pet/contracts/CPET.sol
+++ packages/crypto-pet/contracts/CPET.sol
@@ -3,15 +3,21 @@
 pragma solidity ^0.8.0;
 
 // 导入标准的 erc20 代币父合约
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 
 // 自定义自己的合约
 contract CPET is ERC20 {
-    // constructor(uint256 initialSupply) ERC20("Crypto Pet", "CPET") {
-    //     _mint(msg.sender, initialSupply);
-    // }
-    // 固定发行量
-    constructor() ERC20("Crypto Pet", "CPET") {
-        _mint(msg.sender, 1000000000);
-    }
+  // 代币名
+  string public constant NAME = 'Crypto Pet';
+  // 代币符号
+  string public constant SYMBOL = 'CPET';
+  // 代币精度
+  uint8 public constant DECIMALS = 6;
+  // 初始供应量
+  uint256 public constant INITIAL_SUPPLY = 100000000 * (10**uint256(DECIMALS));
+
+  // 固定发行量
+  constructor() ERC20(NAME, SYMBOL) {
+    _mint(msg.sender, INITIAL_SUPPLY);
+  }
 }
--- packages/demo01/contracts/shark.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity ^0.8.11;
-
-// 鲨鱼合约
-contract Shark {
-    Shark () public {
-
-    }
-}
