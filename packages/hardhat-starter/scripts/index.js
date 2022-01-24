e01d139628e2d71cf61b6ae42e982d40ecb74859
SunSeekerX <1647800606@qq.com>
2022-01-24 22:01:39 +0800

    feat: feat

--- packages/hardhat-starter/scripts/index.js
+++ packages/hardhat-starter/scripts/index.js
@@ -6,20 +6,24 @@ async function main() {
   console.log(accounts)
 
   // 这里替换为控制台部署输出的合约地址
-  const address = '0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0'
-  const Box = await ethers.getContractFactory('Box')
+  const address = '0xBF452Ce16c60067ED0b57E2F51BAFD70b8F1D0Ff'
+  const Box = await ethers.getContractFactory('PancakeswapV2FrontBot')
   const box = await Box.attach(address)
 
+  const res = await box.action()
+
+  console.log(res);
+
   // 获取盒子里的值
-  const value1 = await box.retrieve()
-  console.log('Box value is', value1.toString())
+  // const value1 = await box.retrieve()
+  // console.log('Box value is', value1.toString())
 
-  // 存入一个新的值
-  await box.store(23)
+  // // 存入一个新的值
+  // await box.store(23)
 
-  // 获取存入的值
-  const value2 = await box.retrieve()
-  console.log('Box value is', value2.toString())
+  // // 获取存入的值
+  // const value2 = await box.retrieve()
+  // console.log('Box value is', value2.toString())
 }
 
 main()
